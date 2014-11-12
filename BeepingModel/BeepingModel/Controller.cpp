2a10a9fe86835a4d3fbb0dec69b35abf004d424d
lab.pc <s.y2kmi@gmail.com>
2014-11-12 15:56:50 +0900

    exclusive multimap check updated

--- BeepingModel/BeepingModel/Controller.cpp
+++ BeepingModel/BeepingModel/Controller.cpp
@@ -71,27 +71,40 @@ template <typename C, class T1, class T2> bool findit(const C& c, T1 val1, T2 va
 		return true;
 	}
 }
+
 void Controller::CreateRandomGraph(void)
 {
 	using namespace std;
 	int i = 0;
+	bool selected = false;
 	//M_SIZE edges random selected and add
 	array<int>^ rand_edge = gcnew array<int>(2);
 	multimap<int,int> created_edge;
 	random::mt19937 gen;
 	random::uniform_int_distribution<> dist(0,M_SIZE);
+
 	while(i < M_SIZE){
 		rand_edge[0] = dist(gen);
 		rand_edge[1] = dist(gen);
 		if(rand_edge[0] == rand_edge[1]) continue;
+		//exists channel same endpoint node
+		for(multimap<int,int>::iterator itr = created_edge.begin(); itr != created_edge.end(); ++itr){
+			if( ((*itr).first == rand_edge[0] && (*itr).second == rand_edge[1] )
+				|| ((*itr).first == rand_edge[1] && (*itr).second == rand_edge[0])){
+					selected = true;
+			} else {
+				selected = false;
+			}
+		}
 		//edges already selected
-		if(true){//TODO
+		if(!selected){//TODO
 			channels[i]->EndPoint = rand_edge;
 			created_edge.insert(pair <int, int> (rand_edge[0], rand_edge[1]));
 #ifdef _DEBUG
 			String^ a = String::Format("channel{0}:({1},{2})",i,rand_edge[0],rand_edge[1]);
 			Debug::WriteLine(a);
 #endif
+			selected = false;
 			i++;
 		}else{//edges not selected
 			continue;
